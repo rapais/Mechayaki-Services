6fabed2f0ac3747048db4ef902366555a262fcd6
internship.faiz@bti.id <rafaizghazian@gmail.com>
2026-01-15 01:58:23 +0700

    added image content and css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./router";
 import "./index.css";
 
+import "./styles/vendor/bootstrap.min.css";
+import "./styles/vendor/font-awesome.min.css";
+import "./styles/vendor/animate.css";
+import "./styles/vendor/owl.carousel.css";
+import "./styles/vendor/owl.theme.default.min.css";
+import "./styles/vendor/magnific-popup.css";
+import "./styles/vendor/templatemo-style.css";
+
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: { retry: 1, refetchOnWindowFocus: false },
